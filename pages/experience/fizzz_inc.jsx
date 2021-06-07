943cf21caceb048485f41c091a8188686ee3b9c4
Khadem Avinoor Alam <demon_avi@hotmail.com>
2021-06-06 20:14:25 -0400

    wip

---
+++ pages/experience/fizzz_inc.jsx
@@ -0,0 +1,30 @@
+import Navbar from '../../components/Navbar';
+import Head from 'next/head';
+
+function About() {
+  return (
+    <>
+      <Head>
+        <title>About</title>
+      </Head>
+
+      <Navbar />
+
+      <section className="hero">
+        <div className="container">
+          <div className="text-wrapper w-full">
+            <h1 className="title">About</h1>
+            <p className="description">
+              An interest in Computer Programming led me to do a degree in Computer Science. It took
+              much longer than most, with ups and downs during these seven years. But with
+              persistence and perseverance through repeated failures have finally graduated. Let
+              this be an example how failure is not an obstacle but an opportunity to get stronger.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default About;
